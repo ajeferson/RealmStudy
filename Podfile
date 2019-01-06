c0439f14bc3d68e4ec4dcd6e30a77869cb5a48f8
Alan Jeferson <alan.jeferson@arctouch.com>
2019-01-06 13:10:59 -0200

    Initial commit

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '12.0'
+
+target 'RealmStudy' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  pod 'RealmSwift'
+end
---
+++ RealmStudy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealmStudy
+//
+//  Created by Alan Jeferson on 12/26/18.
+//  Copyright © 2018 Alan Jeferson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
